7e844392fcea8fd56d92e5c2006328b339718611
Nishan Fernando <nfm24@sussex.ac.uk>
2021-02-25 09:51:38 +0000

    IC

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Player
+  autoplay
+  loop
+  src="https://assets3.lottiefiles.com/packages/lf20_UJNc2t.json"
+  style={{ height: '300px', width: '300px' }}
+>
+  <Controls visible={true} buttons={['play', 'repeat', 'frame', 'debug']} />
+</Player>
     </div>
   );
 }
